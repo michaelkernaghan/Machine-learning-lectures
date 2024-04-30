4df1f0442ee439600c52c6de71654f973424c243
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2024-04-30 10:17:44 -0700

    add pythn from jupyter

---
+++ scripts/cs224/exploring-word-vectors-1.py
@@ -0,0 +1,359 @@
+# All Import Statements Defined Here
+# Note: Do not add to this list.
+# ----------------
+
+import sys
+assert sys.version_info[0] == 3
+assert sys.version_info[1] >= 8
+
+from platform import python_version
+assert int(python_version().split(".")[1]) >= 5, "Please upgrade your Python version following the instructions in \
+    the README.md file found in the same directory as this notebook. Your Python version is " + python_version()
+
+from gensim.models import KeyedVectors
+from gensim.test.utils import datapath
+import pprint
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = [10, 5]
+
+import nltk
+nltk.download('reuters') #to specify download location, optionally add the argument: download_dir='/specify/desired/path/'
+from nltk.corpus import reuters
+
+import numpy as np
+import random
+import scipy as sp
+from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import PCA
+
+START_TOKEN = '<START>'
+END_TOKEN = '<END>'
+
+np.random.seed(0)
+random.seed(0)
+# ----------------
+
+def read_corpus(category="gold"):
+    """ Read files from the specified Reuter's category.
+        Params:
+            category (string): category name
+        Return:
+            list of lists, with words from each of the processed files
+    """
+    files = reuters.fileids(category)
+    return [[START_TOKEN] + [w.lower() for w in list(reuters.words(f))] + \
+            [END_TOKEN] for f in files]
+
+reuters_corpus = read_corpus()
+pprint.pprint(reuters_corpus[:3], compact=True, width=100)
+
+def distinct_words(corpus):
+    """ Determine a list of distinct words for the corpus.
+        Params:
+            corpus (list of list of strings): corpus of documents
+        Return:
+            corpus_words (list of strings): sorted list of distinct words across the corpus
+            n_corpus_words (integer): number of distinct words across the corpus
+    """
+    corpus_words = []
+    n_corpus_words = -1
+    
+    # Flatten the list of lists into a single list of words
+    all_words = [word for sublist in corpus for word in sublist]
+    
+    # Use a set to remove duplicates, then convert back to a sorted list
+    corpus_words = sorted(set(all_words))
+    
+    # Count the distinct words
+    n_corpus_words = len(corpus_words)
+    
+    return corpus_words, n_corpus_words
+
+    return corpus_words, n_corpus_words
+
+# ---------------------
+# Run this sanity check
+# Note that this not an exhaustive check for correctness.
+# ---------------------
+
+# Define toy corpus
+test_corpus = ["{} All that glitters isn't gold {}".format(START_TOKEN, END_TOKEN).split(" "), "{} All's well that ends well {}".format(START_TOKEN, END_TOKEN).split(" ")]
+test_corpus_words, num_corpus_words = distinct_words(test_corpus)
+
+# Correct answers
+ans_test_corpus_words = sorted([START_TOKEN, "All", "ends", "that", "gold", "All's", "glitters", "isn't", "well", END_TOKEN])
+ans_num_corpus_words = len(ans_test_corpus_words)
+
+# Test correct number of words
+assert(num_corpus_words == ans_num_corpus_words), "Incorrect number of distinct words. Correct: {}. Yours: {}".format(ans_num_corpus_words, num_corpus_words)
+
+# Test correct words
+assert (test_corpus_words == ans_test_corpus_words), "Incorrect corpus_words.\nCorrect: {}\nYours:   {}".format(str(ans_test_corpus_words), str(test_corpus_words))
+
+# Print Success
+print ("-" * 80)
+print("Passed All Tests!")
+print ("-" * 80)
+
+def compute_co_occurrence_matrix(corpus, window_size=4):
+    """ Compute co-occurrence matrix for the given corpus and window_size (default of 4).
+    
+        Note: Each word in a document should be at the center of a window. Words near edges will have a smaller
+              number of co-occurring words.
+              
+              For example, if we take the document "<START> All that glitters is not gold <END>" with window size of 4,
+              "All" will co-occur with "<START>", "that", "glitters", "is", and "not".
+    
+        Params:
+            corpus (list of list of strings): corpus of documents
+            window_size (int): size of context window
+        Return:
+            M (a symmetric numpy matrix of shape (number of unique words in the corpus , number of unique words in the corpus)): 
+                Co-occurence matrix of word counts. 
+                The ordering of the words in the rows/columns should be the same as the ordering of the words given by the distinct_words function.
+            word2ind (dict): dictionary that maps word to index (i.e. row/column number) for matrix M.
+    """
+    words, n_words = distinct_words(corpus)
+    M = np.zeros((n_words, n_words), dtype=int)
+    word2ind = {word: idx for idx, word in enumerate(words)}
+    
+    # Populate the co-occurrence matrix
+    for doc in corpus:
+        for i, word in enumerate(doc):
+            start = max(0, i - window_size)
+            end = min(len(doc), i + window_size + 1)
+            for j in range(start, end):
+                if i != j:  # Ensure we're not counting the word itself
+                    M[word2ind[word]][word2ind[doc[j]]] += 1
+
+    return M, word2ind
+
+# ---------------------
+# Run this sanity check
+# Note that this is not an exhaustive check for correctness.
+# ---------------------
+
+# Define toy corpus and get student's co-occurrence matrix
+test_corpus = ["{} All that glitters isn't gold {}".format(START_TOKEN, END_TOKEN).split(" "), "{} All's well that ends well {}".format(START_TOKEN, END_TOKEN).split(" ")]
+M_test, word2ind_test = compute_co_occurrence_matrix(test_corpus, window_size=1)
+
+# Correct M and word2ind
+M_test_ans = np.array( 
+    [[0., 0., 0., 0., 0., 0., 1., 0., 0., 1.,],
+     [0., 0., 1., 1., 0., 0., 0., 0., 0., 0.,],
+     [0., 1., 0., 0., 0., 0., 0., 0., 1., 0.,],
+     [0., 1., 0., 0., 0., 0., 0., 0., 0., 1.,],
+     [0., 0., 0., 0., 0., 0., 0., 0., 1., 1.,],
+     [0., 0., 0., 0., 0., 0., 0., 1., 1., 0.,],
+     [1., 0., 0., 0., 0., 0., 0., 1., 0., 0.,],
+     [0., 0., 0., 0., 0., 1., 1., 0., 0., 0.,],
+     [0., 0., 1., 0., 1., 1., 0., 0., 0., 1.,],
+     [1., 0., 0., 1., 1., 0., 0., 0., 1., 0.,]]
+)
+ans_test_corpus_words = sorted([START_TOKEN, "All", "ends", "that", "gold", "All's", "glitters", "isn't", "well", END_TOKEN])
+word2ind_ans = dict(zip(ans_test_corpus_words, range(len(ans_test_corpus_words))))
+
+# Test correct word2ind
+assert (word2ind_ans == word2ind_test), "Your word2ind is incorrect:\nCorrect: {}\nYours: {}".format(word2ind_ans, word2ind_test)
+
+# Test correct M shape
+assert (M_test.shape == M_test_ans.shape), "M matrix has incorrect shape.\nCorrect: {}\nYours: {}".format(M_test.shape, M_test_ans.shape)
+
+# Test correct M values
+for w1 in word2ind_ans.keys():
+    idx1 = word2ind_ans[w1]
+    for w2 in word2ind_ans.keys():
+        idx2 = word2ind_ans[w2]
+        student = M_test[idx1, idx2]
+        correct = M_test_ans[idx1, idx2]
+        if student != correct:
+            print("Correct M:")
+            print(M_test_ans)
+            print("Your M: ")
+            print(M_test)
+            raise AssertionError("Incorrect count at index ({}, {})=({}, {}) in matrix M. Yours has {} but should have {}.".format(idx1, idx2, w1, w2, student, correct))
+
+# Print Success
+print ("-" * 80)
+print("Passed All Tests!")
+print ("-" * 80)
+
+from sklearn.decomposition import TruncatedSVD
+
+def reduce_to_k_dim(M, k=2):
+    """ Reduce a co-occurrence count matrix of dimensionality (num_corpus_words, num_corpus_words)
+        to a matrix of dimensionality (num_corpus_words, k) using the following SVD function from Scikit-Learn:
+            - http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.TruncatedSVD.html
+    
+        Params:
+            M (numpy matrix of shape (number of unique words in the corpus , number of unique words in the corpus)): co-occurrence matrix of word counts
+            k (int): embedding size of each word after dimension reduction
+        Return:
+            M_reduced (numpy matrix of shape (number of corpus words, k)): matrix of k-dimensional word embeddings.
+                    In terms of the SVD from math class, this actually returns U * S
+    """    
+    n_iters = 10    # Use this parameter in your call to `TruncatedSVD`
+    print("Running Truncated SVD over %i words..." % (M.shape[0]))
+
+    # Create a Truncated SVD instance with n_components=k and n_iter=n_iters, then fit it to the matrix M
+    svd = TruncatedSVD(n_components=k, n_iter=n_iters, random_state=42)
+    M_reduced = svd.fit_transform(M)
+
+    print("Done.")
+    return M_reduced
+
+# ---------------------
+# Run this sanity check
+# Note that this is not an exhaustive check for correctness 
+# In fact we only check that your M_reduced has the right dimensions.
+# ---------------------
+
+# Define toy corpus and run student code
+test_corpus = ["{} All that glitters isn't gold {}".format(START_TOKEN, END_TOKEN).split(" "), "{} All's well that ends well {}".format(START_TOKEN, END_TOKEN).split(" ")]
+M_test, word2ind_test = compute_co_occurrence_matrix(test_corpus, window_size=1)
+M_test_reduced = reduce_to_k_dim(M_test, k=2)
+
+# Test proper dimensions
+assert (M_test_reduced.shape[0] == 10), "M_reduced has {} rows; should have {}".format(M_test_reduced.shape[0], 10)
+assert (M_test_reduced.shape[1] == 2), "M_reduced has {} columns; should have {}".format(M_test_reduced.shape[1], 2)
+
+# Print Success
+print ("-" * 80)
+print("Passed All Tests!")
+print ("-" * 80)
+
+from datetime import datetime 
+def plot_embeddings(M_reduced, word2ind, words, filename='plot'):
+    """ Plot in a scatterplot the embeddings of the words specified in the list "words" and save the plot to a file with a timestamp.
+        NOTE: do not plot all the words listed in M_reduced / word2ind.
+        Include a label next to each point.
+        
+        Params:
+            M_reduced (numpy matrix of shape (number of unique words in the corpus , 2)): matrix of 2-dimensional word embeddings
+            word2ind (dict): dictionary that maps word to indices for matrix M
+            words (list of strings): words whose embeddings we want to visualize
+            filename (str): base filename for the output plot image, timestamp and extension will be appended
+    """
+    # Generate timestamp
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    full_filename = f"{filename}_{timestamp}.png"
+
+    # Extract the coordinates for the words to plot
+    x_coords = [M_reduced[word2ind[word], 0] for word in words if word in word2ind]
+    y_coords = [M_reduced[word2ind[word], 1] for word in words if word in word2ind]
+
+    # Create a scatter plot
+    plt.figure(figsize=(8, 5))
+    plt.scatter(x_coords, y_coords, color='red')
+
+    # Annotate each point in the scatter plot with its word label
+    for word, x, y in zip(words, x_coords, y_coords):
+        plt.annotate(word, (x, y), textcoords="offset points", xytext=(0,10), ha='center')
+
+    # Set title and labels for better readability
+    plt.title('Word Embeddings Visualized')
+    plt.xlabel('Dimension 1')
+    plt.ylabel('Dimension 2')
+    plt.grid(True)
+
+    # Save the plot to a file
+    plt.savefig(full_filename)
+    plt.close()  # Close the plot figure to free up memory
+    print("Plot saved to", full_filename)
+
+# ---------------------
+# Run this sanity check
+# Note that this is not an exhaustive check for correctness.
+# The plot produced should look like the included file question_1.4_test.png 
+# ---------------------
+
+print ("-" * 80)
+print ("Outputted Plot:")
+
+M_reduced_plot_test = np.array([[1, 1], [-1, -1], [1, -1], [-1, 1], [0, 0]])
+word2ind_plot_test = {'test1': 0, 'test2': 1, 'test3': 2, 'test4': 3, 'test5': 4}
+words = ['test1', 'test2', 'test3', 'test4', 'test5']
+plot_embeddings(M_reduced_plot_test, word2ind_plot_test, words)
+
+print ("-" * 80)
+
+# -----------------------------
+# Run This Cell to Produce Your Plot
+# ------------------------------
+reuters_corpus = read_corpus()
+M_co_occurrence, word2ind_co_occurrence = compute_co_occurrence_matrix(reuters_corpus)
+M_reduced_co_occurrence = reduce_to_k_dim(M_co_occurrence, k=2)
+
+# Rescale (normalize) the rows to make them each of unit-length
+M_lengths = np.linalg.norm(M_reduced_co_occurrence, axis=1)
+M_normalized = M_reduced_co_occurrence / M_lengths[:, np.newaxis] # broadcasting
+
+words = ['value', 'gold', 'platinum', 'reserves', 'silver', 'metals', 'copper', 'belgium', 'australia', 'china', 'grammes', "mine"]
+
+plot_embeddings(M_normalized, word2ind_co_occurrence, words)
+
+def load_embedding_model():
+    """ Load GloVe Vectors
+        Return:
+            wv_from_bin: All 400000 embeddings, each lengh 200
+    """
+    import gensim.downloader as api
+    wv_from_bin = api.load("glove-wiki-gigaword-200")
+    print("Loaded vocab size %i" % len(list(wv_from_bin.index_to_key)))
+    return wv_from_bin
+wv_from_bin = load_embedding_model()
+
+def get_matrix_of_vectors(wv_from_bin, required_words):
+    """ Put the GloVe vectors into a matrix M.
+        Param:
+            wv_from_bin: KeyedVectors object; the 400000 GloVe vectors loaded from file
+        Return:
+            M: numpy matrix shape (num words, 200) containing the vectors
+            word2ind: dictionary mapping each word to its row number in M
+    """
+    import random
+    words = list(wv_from_bin.index_to_key)
+    print("Shuffling words ...")
+    random.seed(225)
+    random.shuffle(words)
+    words = words[:10000]
+    print("Putting %i words into word2ind and matrix M..." % len(words))
+    word2ind = {}
+    M = []
+    curInd = 0
+    for w in words:
+        try:
+            M.append(wv_from_bin.get_vector(w))
+            word2ind[w] = curInd
+            curInd += 1
+        except KeyError:
+            continue
+    for w in required_words:
+        if w in words:
+            continue
+        try:
+            M.append(wv_from_bin.get_vector(w))
+            word2ind[w] = curInd
+            curInd += 1
+        except KeyError:
+            continue
+    M = np.stack(M)
+    print("Done.")
+    return M, word2ind
+
+# -----------------------------------------------------------------
+# Run Cell to Reduce 200-Dimensional Word Embeddings to k Dimensions
+# Note: This should be quick to run
+# -----------------------------------------------------------------
+M, word2ind = get_matrix_of_vectors(wv_from_bin, words)
+M_reduced = reduce_to_k_dim(M, k=2)
+
+# Rescale (normalize) the rows to make them each of unit-length
+M_lengths = np.linalg.norm(M_reduced, axis=1)
+M_reduced_normalized = M_reduced / M_lengths[:, np.newaxis] # broadcasting
+
+words = ['value', 'gold', 'platinum', 'reserves', 'silver', 'metals', 'copper', 'belgium', 'australia', 'china', 'grammes', "mine"]
+
+plot_embeddings(M_reduced_normalized, word2ind, words)
+
